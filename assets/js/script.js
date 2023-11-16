f1aaa23f91f49cba81586f13a001e96d7cb81fba
Brazz26 <147454142+Brazz26@users.noreply.github.com>
2023-11-15 20:31:20 -0500

    inserted bootstrap stylings for simple inline css

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var beginScreen = document.querySelector('.begin');
+var quizScreen = document.querySelector('.quiz');
+var endScreen = document.querySelector('.end');
+var startBtn = document.querySelector('#.start-Btn')
+var score = 100
