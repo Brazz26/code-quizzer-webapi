2b2298a5a8d7b2f2c63a3c0003d1fb3d4305c943
Brazz26 <147454142+Brazz26@users.noreply.github.com>
2023-12-02 00:32:55 -0500

    added questions to each section of coding quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var beginScreen = document.querySelector('.begin');
 var quizScreen = document.querySelector('.quiz');
-var endScreen = document.querySelector('.end');
+var finishScreen = document.querySelector('.finish');
 var startBtn = document.querySelector('#.start-Btn')
 var score = 100
+
+function ()
